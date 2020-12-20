11422ef3265131aef26e23d4d34e78c182656047
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-20 21:27:48 +0200

    props to post

--- App.js
+++ App.js
@@ -10,8 +10,11 @@ import React from 'react';
 import {SafeAreaView, ScrollView, View, Text, StatusBar} from 'react-native';
 import HomeScreen from './src/screens/Home';
 import Post from './src/components/Post';
-
 import Entypo from 'react-native-vector-icons/Entypo';
+
+import feed from './assets/data/feed';
+
+const post1 = feed[0];
 
 const App: () => React$Node = () => {
   return (
@@ -19,7 +22,7 @@ const App: () => React$Node = () => {
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         {/* <HomeScreen /> */}
-        <Post />
+        <Post post={post1} />
       </SafeAreaView>
     </>
   );
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,26 +2,27 @@ import React from 'react';
 import {View, Text, Image} from 'react-native';
 import styles from './styles';
 
-const Post = () => {
+const Post = (props) => {
+  const post = props.post;
   return (
     <View style={styles.container}>
       <Image
         source={{
-          uri:
-            'https://www.channelnewsasia.com/image/12661872/4x3/600/450/6fa39e9b609766dbc7291f8daf7666c6/Of/onefinestay-june-st-barts-.jpg',
+          uri: post.image,
         }}
         style={styles.image}
       />
-      <Text style={styles.bedrooms}>1 bed 1 bedroom</Text>
+      <Text style={styles.bedrooms}>
+        {post.bed} bed {post.bedroom} bedroom
+      </Text>
       <Text style={styles.desc} numberOfLines={2}>
-        Entire flat, lorem aspectm lorem aspectm lorem aspectm lorem aspectm
-        lorem aspectm lorem aspectm lorem aspectm lorem aspectm
+        {post.type}, {post.title}
       </Text>
       <Text style={styles.prices}>
-        <Text style={styles.oldPrice}>$36</Text>
-        <Text style={styles.newPrice}> $30 </Text>/ night
+        <Text style={styles.oldPrice}>${post.oldPrice}</Text>
+        <Text style={styles.newPrice}> ${post.newPrice} </Text>/ night
       </Text>
-      <Text style={styles.totalPrice}>$230 Total</Text>
+      <Text style={styles.totalPrice}>${post.totalPrice} Total</Text>
     </View>
   );
 };
