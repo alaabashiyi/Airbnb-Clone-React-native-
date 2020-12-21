436e6661fc04135e4caa98dee5b0396ee7c336f0
Alaa Bashiyi <alaabashiyi@Alaas-iMac.local>
2020-12-21 03:07:10 +0200

    google maps on ios and android

--- src/navigation/SearchResultsTabNavigator.js
+++ src/navigation/SearchResultsTabNavigator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import SearchResults from '../screens/SearchResults';
-// import SearchResultsMap from '../screens/SearchResultsMap';
+import SearchResultsMap from '../screens/SearchResultsMap';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -15,7 +15,7 @@ const SearchResultsTabNavigator = (props) => {
         },
       }}>
       <Tab.Screen name={'list'} component={SearchResults} />
-      <Tab.Screen name={'map'} component={SearchResults} />
+      <Tab.Screen name={'map'} component={SearchResultsMap} />
     </Tab.Navigator>
   );
 };
